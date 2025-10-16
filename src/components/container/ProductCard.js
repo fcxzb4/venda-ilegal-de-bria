f0a55c6851590634c2a0a4fb36b1cc519a9d7ede
Bridget <84099228+fcxzb4@users.noreply.github.com>
2025-10-15 21:34:12 -0300

    ssd

--- src/components/container/ProductCard.js
+++ src/components/container/ProductCard.js
@@ -82,7 +82,7 @@ function ProductCard({ product, onUpdate }) {
       // 2. Requisição DELETE
       // ⚠️ GARANTA QUE O ID ESTÁ SENDO INSERIDO CORRETAMENTE NA URL
       const response = await fetch(
-       `http://localhost:3001/card`,
+       `http://localhost:3001/card  `,
         {
           method: "DELETE",
         }
